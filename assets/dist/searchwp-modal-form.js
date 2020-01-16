d382ec354b33e614f88ec1eae422f6c19aea39c9
Jon Christopher <jon@jonchristopher.us>
2020-01-16 11:44:15 -0500

    Rebuilds assets

--- assets/dist/searchwp-modal-form.js
+++ assets/dist/searchwp-modal-form.js
@@ -125,206 +125,317 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var version = "0.3.2",
-    classCallCheck = function (e, o) {
-  if (!(e instanceof o)) throw new TypeError("Cannot call a class as a function");
-},
-    createClass = function () {
-  function e(e, o) {
-    for (var t = 0; t < o.length; t++) {
-      var n = o[t];
-      n.enumerable = n.enumerable || !1, n.configurable = !0, "value" in n && (n.writable = !0), Object.defineProperty(e, n.key, n);
+const MicroModal = (() => {
+  const FOCUSABLE_ELEMENTS = ['a[href]', 'area[href]', 'input:not([disabled]):not([type="hidden"]):not([aria-hidden])', 'select:not([disabled]):not([aria-hidden])', 'textarea:not([disabled]):not([aria-hidden])', 'button:not([disabled]):not([aria-hidden])', 'iframe', 'object', 'embed', '[contenteditable]', '[tabindex]:not([tabindex^="-"])'];
+
+  class Modal {
+    constructor({
+      targetModal,
+      triggers = [],
+      onShow = () => {},
+      onClose = () => {},
+      openTrigger = 'data-micromodal-trigger',
+      closeTrigger = 'data-micromodal-close',
+      disableScroll = false,
+      disableFocus = false,
+      awaitCloseAnimation = false,
+      awaitOpenAnimation = false,
+      debugMode = false
+    }) {
+      // Save a reference of the modal
+      this.modal = document.getElementById(targetModal); // Save a reference to the passed config
+
+      this.config = {
+        debugMode,
+        disableScroll,
+        openTrigger,
+        closeTrigger,
+        onShow,
+        onClose,
+        awaitCloseAnimation,
+        awaitOpenAnimation,
+        disableFocus // Register click events only if pre binding eventListeners
+
+      };
+      if (triggers.length > 0) this.registerTriggers(...triggers); // pre bind functions for event listeners
+
+      this.onClick = this.onClick.bind(this);
+      this.onKeydown = this.onKeydown.bind(this);
     }
-  }
+    /**
+     * Loops through all openTriggers and binds click event
+     * @param  {array} triggers [Array of node elements]
+     * @return {void}
+     */
 
-  return function (o, t, n) {
-    return t && e(o.prototype, t), n && e(o, n), o;
-  };
-}(),
-    toConsumableArray = function (e) {
-  if (Array.isArray(e)) {
-    for (var o = 0, t = Array(e.length); o < e.length; o++) t[o] = e[o];
 
-    return t;
-  }
-
-  return Array.from(e);
-},
-    MicroModal = function () {
-  var e = ["a[href]", "area[href]", 'input:not([disabled]):not([type="hidden"]):not([aria-hidden])', "select:not([disabled]):not([aria-hidden])", "textarea:not([disabled]):not([aria-hidden])", "button:not([disabled]):not([aria-hidden])", "iframe", "object", "embed", "[contenteditable]", '[tabindex]:not([tabindex^="-"])'],
-      o = function () {
-    function o(e) {
-      var t = e.targetModal,
-          n = e.triggers,
-          i = void 0 === n ? [] : n,
-          a = e.onShow,
-          r = void 0 === a ? function () {} : a,
-          s = e.onClose,
-          l = void 0 === s ? function () {} : s,
-          c = e.openTrigger,
-          d = void 0 === c ? "data-micromodal-trigger" : c,
-          u = e.closeTrigger,
-          h = void 0 === u ? "data-micromodal-close" : u,
-          f = e.disableScroll,
-          v = void 0 !== f && f,
-          m = e.disableFocus,
-          g = void 0 !== m && m,
-          b = e.awaitCloseAnimation,
-          y = void 0 !== b && b,
-          k = e.debugMode,
-          w = void 0 !== k && k;
-      classCallCheck(this, o), this.modal = document.getElementById(t), this.config = {
-        debugMode: w,
-        disableScroll: v,
-        openTrigger: d,
-        closeTrigger: h,
-        onShow: r,
-        onClose: l,
-        awaitCloseAnimation: y,
-        disableFocus: g
-      }, i.length > 0 && this.registerTriggers.apply(this, toConsumableArray(i)), this.onClick = this.onClick.bind(this), this.onKeydown = this.onKeydown.bind(this);
+    registerTriggers(...triggers) {
+      triggers.filter(Boolean).forEach(trigger => {
+        trigger.addEventListener('click', event => this.showModal(event));
+      });
     }
 
-    return createClass(o, [{
-      key: "registerTriggers",
-      value: function () {
-        for (var e = this, o = arguments.length, t = Array(o), n = 0; n < o; n++) t[n] = arguments[n];
+    showModal() {
+      this.activeElement = document.activeElement;
+      this.modal.setAttribute('aria-hidden', 'false');
+      this.modal.classList.add('is-open');
+      this.scrollBehaviour('disable');
+      this.addEventListeners();
 
-        t.filter(Boolean).forEach(function (o) {
-          o.addEventListener("click", function () {
-            return e.showModal();
+      if (this.config.awaitOpenAnimation) {
+        const handler = () => {
+          this.modal.removeEventListener('animationend', handler, false);
+          this.setFocusToFirstNode();
+        };
+
+        this.modal.addEventListener('animationend', handler, false);
+      } else {
+        this.setFocusToFirstNode();
+      }
+
+      this.config.onShow(this.modal, this.activeElement);
+    }
+
+    closeModal() {
+      const modal = this.modal;
+      this.modal.setAttribute('aria-hidden', 'true');
+      this.removeEventListeners();
+      this.scrollBehaviour('enable');
+
+      if (this.activeElement) {
+        this.activeElement.focus();
+      }
+
+      this.config.onClose(this.modal);
+
+      if (this.config.awaitCloseAnimation) {
+        this.modal.addEventListener('animationend', function handler() {
+          modal.classList.remove('is-open');
+          modal.removeEventListener('animationend', handler, false);
+        }, false);
+      } else {
+        modal.classList.remove('is-open');
+      }
+    }
+
+    closeModalById(targetModal) {
+      this.modal = document.getElementById(targetModal);
+      if (this.modal) this.closeModal();
+    }
+
+    scrollBehaviour(toggle) {
+      if (!this.config.disableScroll) return;
+      const body = document.querySelector('body');
+
+      switch (toggle) {
+        case 'enable':
+          Object.assign(body.style, {
+            overflow: '',
+            height: ''
           });
-        });
-      }
-    }, {
-      key: "showModal",
-      value: function () {
-        this.activeElement = document.activeElement, this.modal.setAttribute("aria-hidden", "false"), this.modal.classList.add("is-open"), this.setFocusToFirstNode(), this.scrollBehaviour("disable"), this.addEventListeners(), this.config.onShow(this.modal);
-      }
-    }, {
-      key: "closeModal",
-      value: function () {
-        var e = this.modal;
-        this.modal.setAttribute("aria-hidden", "true"), this.removeEventListeners(), this.scrollBehaviour("enable"), this.activeElement && this.activeElement.focus(), this.config.onClose(this.modal), this.config.awaitCloseAnimation ? this.modal.addEventListener("animationend", function o() {
-          e.classList.remove("is-open"), e.removeEventListener("animationend", o, !1);
-        }, !1) : e.classList.remove("is-open");
-      }
-    }, {
-      key: "closeModalById",
-      value: function (e) {
-        this.modal = document.getElementById(e), this.modal && this.closeModal();
-      }
-    }, {
-      key: "scrollBehaviour",
-      value: function (e) {
-        if (this.config.disableScroll) {
-          var o = document.querySelector("body");
+          break;
 
-          switch (e) {
-            case "enable":
-              Object.assign(o.style, {
-                overflow: "",
-                height: ""
-              });
-              break;
+        case 'disable':
+          Object.assign(body.style, {
+            overflow: 'hidden',
+            height: '100vh'
+          });
+          break;
 
-            case "disable":
-              Object.assign(o.style, {
-                overflow: "hidden",
-                height: "100vh"
-              });
-          }
+        default:
+      }
+    }
+
+    addEventListeners() {
+      this.modal.addEventListener('touchstart', this.onClick);
+      this.modal.addEventListener('click', this.onClick);
+      document.addEventListener('keydown', this.onKeydown);
+    }
+
+    removeEventListeners() {
+      this.modal.removeEventListener('touchstart', this.onClick);
+      this.modal.removeEventListener('click', this.onClick);
+      document.removeEventListener('keydown', this.onKeydown);
+    }
+
+    onClick(event) {
+      if (event.target.hasAttribute(this.config.closeTrigger)) {
+        this.closeModal();
+        event.preventDefault();
+      }
+    }
+
+    onKeydown(event) {
+      if (event.keyCode === 27) this.closeModal(event);
+      if (event.keyCode === 9) this.maintainFocus(event);
+    }
+
+    getFocusableNodes() {
+      const nodes = this.modal.querySelectorAll(FOCUSABLE_ELEMENTS);
+      return Array(...nodes);
+    }
+
+    setFocusToFirstNode() {
+      if (this.config.disableFocus) return;
+      const focusableNodes = this.getFocusableNodes();
+      if (focusableNodes.length) focusableNodes[0].focus();
+    }
+
+    maintainFocus(event) {
+      const focusableNodes = this.getFocusableNodes(); // if disableFocus is true
+
+      if (!this.modal.contains(document.activeElement)) {
+        focusableNodes[0].focus();
+      } else {
+        const focusedItemIndex = focusableNodes.indexOf(document.activeElement);
+
+        if (event.shiftKey && focusedItemIndex === 0) {
+          focusableNodes[focusableNodes.length - 1].focus();
+          event.preventDefault();
+        }
+
+        if (!event.shiftKey && focusedItemIndex === focusableNodes.length - 1) {
+          focusableNodes[0].focus();
+          event.preventDefault();
         }
       }
-    }, {
-      key: "addEventListeners",
-      value: function () {
-        this.modal.addEventListener("touchstart", this.onClick), this.modal.addEventListener("click", this.onClick), document.addEventListener("keydown", this.onKeydown);
-      }
-    }, {
-      key: "removeEventListeners",
-      value: function () {
-        this.modal.removeEventListener("touchstart", this.onClick), this.modal.removeEventListener("click", this.onClick), document.removeEventListener("keydown", this.onKeydown);
-      }
-    }, {
-      key: "onClick",
-      value: function (e) {
-        e.target.hasAttribute(this.config.closeTrigger) && (this.closeModal(), e.preventDefault());
-      }
-    }, {
-      key: "onKeydown",
-      value: function (e) {
-        27 === e.keyCode && this.closeModal(e), 9 === e.keyCode && this.maintainFocus(e);
-      }
-    }, {
-      key: "getFocusableNodes",
-      value: function () {
-        var o = this.modal.querySelectorAll(e);
-        return Array.apply(void 0, toConsumableArray(o));
-      }
-    }, {
-      key: "setFocusToFirstNode",
-      value: function () {
-        if (!this.config.disableFocus) {
-          var e = this.getFocusableNodes();
-          e.length && e[0].focus();
-        }
-      }
-    }, {
-      key: "maintainFocus",
-      value: function (e) {
-        var o = this.getFocusableNodes();
+    }
 
-        if (this.modal.contains(document.activeElement)) {
-          var t = o.indexOf(document.activeElement);
-          e.shiftKey && 0 === t && (o[o.length - 1].focus(), e.preventDefault()), e.shiftKey || t !== o.length - 1 || (o[0].focus(), e.preventDefault());
-        } else o[0].focus();
-      }
-    }]), o;
-  }(),
-      t = null,
-      n = function (e, o) {
-    var t = [];
-    return e.forEach(function (e) {
-      var n = e.attributes[o].value;
-      void 0 === t[n] && (t[n] = []), t[n].push(e);
-    }), t;
-  },
-      i = function (e) {
-    if (!document.getElementById(e)) return console.warn("MicroModal v" + version + ": ❗Seems like you have missed %c'" + e + "'", "background-color: #f8f9fa;color: #50596c;font-weight: bold;", "ID somewhere in your code. Refer example below to resolve it."), console.warn("%cExample:", "background-color: #f8f9fa;color: #50596c;font-weight: bold;", '<div class="modal" id="' + e + '"></div>'), !1;
-  },
-      a = function (e) {
-    if (e.length <= 0) return console.warn("MicroModal v" + version + ": ❗Please specify at least one %c'micromodal-trigger'", "background-color: #f8f9fa;color: #50596c;font-weight: bold;", "data attribute."), console.warn("%cExample:", "background-color: #f8f9fa;color: #50596c;font-weight: bold;", '<a href="#" data-micromodal-trigger="my-modal"></a>'), !1;
-  },
-      r = function (e, o) {
-    if (a(e), !o) return !0;
+  }
+  /**
+   * Modal prototype ends.
+   * Here on code is responsible for detecting and
+   * auto binding event handlers on modal triggers
+   */
+  // Keep a reference to the opened modal
 
-    for (var t in o) i(t);
 
-    return !0;
+  let activeModal = null;
+  /**
+   * Generates an associative array of modals and it's
+   * respective triggers
+   * @param  {array} triggers     An array of all triggers
+   * @param  {string} triggerAttr The data-attribute which triggers the module
+   * @return {array}
+   */
+
+  const generateTriggerMap = (triggers, triggerAttr) => {
+    const triggerMap = [];
+    triggers.forEach(trigger => {
+      const targetModal = trigger.attributes[triggerAttr].value;
+      if (triggerMap[targetModal] === undefined) triggerMap[targetModal] = [];
+      triggerMap[targetModal].push(trigger);
+    });
+    return triggerMap;
+  };
+  /**
+   * Validates whether a modal of the given id exists
+   * in the DOM
+   * @param  {number} id  The id of the modal
+   * @return {boolean}
+   */
+
+
+  const validateModalPresence = id => {
+    if (!document.getElementById(id)) {
+      console.warn(`MicroModal: \u2757Seems like you have missed %c'${id}'`, 'background-color: #f8f9fa;color: #50596c;font-weight: bold;', 'ID somewhere in your code. Refer example below to resolve it.');
+      console.warn(`%cExample:`, 'background-color: #f8f9fa;color: #50596c;font-weight: bold;', `<div class="modal" id="${id}"></div>`);
+      return false;
+    }
+  };
+  /**
+   * Validates if there are modal triggers present
+   * in the DOM
+   * @param  {array} triggers An array of data-triggers
+   * @return {boolean}
+   */
+
+
+  const validateTriggerPresence = triggers => {
+    if (triggers.length <= 0) {
+      console.warn(`MicroModal: \u2757Please specify at least one %c'micromodal-trigger'`, 'background-color: #f8f9fa;color: #50596c;font-weight: bold;', 'data attribute.');
+      console.warn(`%cExample:`, 'background-color: #f8f9fa;color: #50596c;font-weight: bold;', `<a href="#" data-micromodal-trigger="my-modal"></a>`);
+      return false;
+    }
+  };
+  /**
+   * Checks if triggers and their corresponding modals
+   * are present in the DOM
+   * @param  {array} triggers   Array of DOM nodes which have data-triggers
+   * @param  {array} triggerMap Associative array of modals and their triggers
+   * @return {boolean}
+   */
+
+
+  const validateArgs = (triggers, triggerMap) => {
+    validateTriggerPresence(triggers);
+    if (!triggerMap) return true;
+
+    for (var id in triggerMap) validateModalPresence(id);
+
+    return true;
+  };
+  /**
+   * Binds click handlers to all modal triggers
+   * @param  {object} config [description]
+   * @return void
+   */
+
+
+  const init = config => {
+    // Create an config object with default openTrigger
+    const options = Object.assign({}, {
+      openTrigger: 'data-micromodal-trigger'
+    }, config); // Collects all the nodes with the trigger
+
+    const triggers = [...document.querySelectorAll(`[${options.openTrigger}]`)]; // Makes a mappings of modals with their trigger nodes
+
+    const triggerMap = generateTriggerMap(triggers, options.openTrigger); // Checks if modals and triggers exist in dom
+
+    if (options.debugMode === true && validateArgs(triggers, triggerMap) === false) return; // For every target modal creates a new instance
+
+    for (var key in triggerMap) {
+      let value = triggerMap[key];
+      options.targetModal = key;
+      options.triggers = [...value];
+      activeModal = new Modal(options); // eslint-disable-line no-new
+    }
+  };
+  /**
+   * Shows a particular modal
+   * @param  {string} targetModal [The id of the modal to display]
+   * @param  {object} config [The configuration object to pass]
+   * @return {void}
+   */
+
+
+  const show = (targetModal, config) => {
+    const options = config || {};
+    options.targetModal = targetModal; // Checks if modals and triggers exist in dom
+
+    if (options.debugMode === true && validateModalPresence(targetModal) === false) return; // stores reference to active modal
+
+    activeModal = new Modal(options); // eslint-disable-line no-new
+
+    activeModal.showModal();
+  };
+  /**
+   * Closes the active modal
+   * @param  {string} targetModal [The id of the modal to close]
+   * @return {void}
+   */
+
+
+  const close = targetModal => {
+    targetModal ? activeModal.closeModalById(targetModal) : activeModal.closeModal();
   };
 
   return {
-    init: function (e) {
-      var t = Object.assign({}, {
-        openTrigger: "data-micromodal-trigger"
-      }, e),
-          i = [].concat(toConsumableArray(document.querySelectorAll("[" + t.openTrigger + "]"))),
-          a = n(i, t.openTrigger);
-      if (!0 !== t.debugMode || !1 !== r(i, a)) for (var s in a) {
-        var l = a[s];
-        t.targetModal = s, t.triggers = [].concat(toConsumableArray(l)), new o(t);
-      }
-    },
-    show: function (e, n) {
-      var a = n || {};
-      a.targetModal = e, !0 === a.debugMode && !1 === i(e) || (t = new o(a)).showModal();
-    },
-    close: function (e) {
-      e ? t.closeModalById(e) : t.closeModal();
-    }
+    init,
+    show,
+    close
   };
-}();
+})();
 
 var _default = MicroModal;
 exports.default = _default;
@@ -390,7 +501,26 @@ _micromodal.default.init({
       var modal = event.currentTarget.getAttribute("data-searchwp-modal-trigger");
       showing = modal;
 
-      _micromodal.default.show(modal);
+      _micromodal.default.show(modal, {
+        onShow: function onShow(obj) {
+          if (window.jQuery) {
+            jQuery('body').trigger('onShow', {
+              modal: modal,
+              el: jQuery('#' + modal),
+              obj: obj
+            });
+          }
+        },
+        onClose: function onClose(obj) {
+          if (window.jQuery) {
+            jQuery('body').trigger('onClose', {
+              modal: modal,
+              el: jQuery('#' + modal),
+              obj: obj
+            });
+          }
+        }
+      });
     }, false);
   });
   closers.forEach(function (closer) {
